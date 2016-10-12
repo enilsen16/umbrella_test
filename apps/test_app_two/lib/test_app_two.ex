d25575f39be4fb332a2edf7e5d0a127fb5af46dc
Erik Nilsen <enilsen16@live.com>
2016-10-11 21:14:55 -0700

    initial commit

---
+++ apps/test_app_two/lib/test_app_two.ex
@@ -0,0 +1,18 @@
+defmodule TestAppTwo do
+  @moduledoc """
+  Documentation for TestAppTwo.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> TestAppTwo.hello
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ apps/test_app_two/test/test_app_two_test.exs
@@ -0,0 +1,8 @@
+defmodule TestAppTwoTest do
+  use ExUnit.Case
+  doctest TestAppTwo
+
+  test "the truth" do
+    assert 1 + 1 == 2
+  end
+end
